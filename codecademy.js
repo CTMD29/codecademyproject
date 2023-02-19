6e218e3018690c2d468839887e212c6655a9671a
Charlie Double <charliedouble@charlies-air.home>
2023-02-19 15:39:29 +0000

    first commit

---
+++ codecademy.js
@@ -0,0 +1,21 @@
+let num = Math.floor(Math.random()*5+1);
+
+console.log(num);
+
+switch(num){
+    case 1:
+        console.log('you will have a happy life');
+        break;
+
+    case 2:
+        console.log('you will have a sad life');
+        break;
+
+    case 3:
+        console.log('you will have good luck');
+        break;
+
+    case 4:
+        console.log('you will have bad luck');
+        break;
+}
